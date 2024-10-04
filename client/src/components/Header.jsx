5a24b5c033be607c10e952f53e4149c2dd52cf98
Acxel Montoya <acxelfer1@gmail.com>
2024-10-04 17:54:56 -0600

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
               <FaMoon />  
             </Button>
             <Link to='/sign-in'>
-                <Button gradientDuoTone='purpleToBlue'>
+                <Button gradientDuoTone='purpleToBlue' outline>
                     Registrarse
                 </Button>
             </Link>
