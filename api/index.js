7326c79917113c3c7a3fee97e1e563275281dd2b
Acxel Montoya <acxelfer1@gmail.com>
2024-10-04 22:06:48 -0600

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRotes from './routes/user.route.js';
+import authRotes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,8 +15,11 @@ mongoose.connect(process.env.MONGO).then(
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('El servidor esta corriendo en el puerto 3000!');
 });
 
 app.use('/api/user', userRotes);
+app.use('/api/auth', authRotes);
