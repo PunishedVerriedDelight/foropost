7f9407e9e797bcc51d91d6ac77e34170e6747f45
Acxel Montoya <acxelfer1@gmail.com>
2024-10-12 20:01:33 -0600

    create and add the footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {BsFacebook, BsTwitterX, BsGithub, BsDiscord} from 'react-icons/bs'
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-around sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl 
+                    font-semibold dark:text-white"
+            >
+              <span
+                className="px-2 py-1 bg-gradient-to-r from-orange-500
+                     to-yellow-300 rounded-lg text-white"
+              >
+                FOROPOST
+              </span>
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+            <div>
+              <Footer.Title title="Sobre" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.fcfm.uanl.mx/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  FCFM
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Foropost
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Siguenos en" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://github.com/PunishedVerriedDelight/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link
+                  href="discordapp.com/usuarios/cxlea"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Discord
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title="Información Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://www.uanl.mx/aviso-de-privacidad/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Politicas de privacidad
+                </Footer.Link>
+                <Footer.Link
+                  href="http://transparencia.uanl.mx/normatividad_vigente/leyesYreg.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Terminos y condiciones
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider/>
+          <div className="w-full sm:flex sm:items-center sm:justify-between">
+            <Footer.Copyright
+              href="#"
+              by="FOROPOST"
+              year={new Date().getFullYear()}
+            />
+            <div className="flex gap-3 sm:mt-0 mt-4 sm:justify-center">
+                <Footer.Icon href="https://www.facebook.com/?locale=es_LA" icon={BsFacebook}/>
+                <Footer.Icon href="https://x.com/?lang=es" icon={BsTwitterX}/>
+                <Footer.Icon href="https://github.com/PunishedVerriedDelight/" icon={BsDiscord}/>
+            </div>
+          </div>
+      </div>
+    </Footer>
+  );
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,14 +7,40 @@ export default function SignUp() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return passwordPattern.test(password);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (!formData.username || !formData.email || !formData.password) {
-      return setErrorMessage('Por favor llene los espacios vacios');
+      return setErrorMessage("Por favor llene los espacios vacios");
     }
+
+    if (!validateEmail(formData.email)) {
+      return setErrorMessage(
+        "Por favor ingrese un email en el formato correcto"
+      );
+    }
+
+    if (!validatePassword(formData.password)) {
+      return setErrorMessage(
+        "La contraseña debe tener al menos 8 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un carácter especial."
+      );
+    }
+
     try {
       setLoading(true);
       setErrorMessage(null);
@@ -25,34 +51,34 @@ export default function SignUp() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        setLoading(false);
+        return setErrorMessage(
+          "El nombre de usuario y/o correo electrónico ya han sido registrados"
+        );
       }
       setLoading(false);
-      if(res.ok) {
-        navigate('/sign-in');
+      if (res.ok) {
+        navigate("/sign-in");
       }
     } catch (error) {
       setErrorMessage(error.message);
       setLoading(false);
     }
   };
-  console.log(formData);
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span
-              className="px-2 py-1 bg-gradient-to-r from-orange-500
-            to-yellow-300 rounded-lg text-white"
-            >
+            <span className="px-2 py-1 bg-gradient-to-r from-orange-500 to-yellow-300 rounded-lg text-white">
               FOROPOST
             </span>
           </Link>
           <p className="text-sm mt-5">
-            Crea posts y compartelos con tus amigos y comunidades. Comenta y
-            dialoga sobre temas de tu interes.
+            Crea posts y compártelos con tus amigos y comunidades. Comenta y
+            dialoga sobre temas de tu interés.
           </p>
         </div>
         {/* right */}
@@ -61,7 +87,7 @@ export default function SignUp() {
             <div>
               <Label value="Usuario" />
               <TextInput
-                type="text"
+                type="username"
                 placeholder="Nombre de usuario"
                 id="username"
                 onChange={handleChange}
@@ -85,15 +111,19 @@ export default function SignUp() {
                 onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone="pinkToOrange" type="submit" disabled={loading}>
-              {
-                loading ? (
-                  <>
-                  <Spinner size='sm'/>
-                  <span className='pl-3'>Cargando...</span>
-                  </>
-                ) : 'Registrarse'
-              }
+            <Button
+              gradientDuoTone="pinkToOrange"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Cargando...</span>
+                </>
+              ) : (
+                "Registrarse"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -102,13 +132,11 @@ export default function SignUp() {
               Ingresar
             </Link>
           </div>
-          {
-           errorMessage && (
-            <Alert className='mt-5 ' color='failure'>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
               {errorMessage}
             </Alert>
-          ) 
-          }
+          )}
         </div>
       </div>
     </div>
