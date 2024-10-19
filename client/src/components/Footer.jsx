87be52bde061e6aed3d1d9110ce87139d2061dfc
PunishedVerriedDelight <acxelfer1@gmail.com>
2024-10-19 01:59:23 -0600

    add admin functionality to the user

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import {BsFacebook, BsTwitterX, BsGithub, BsDiscord} from 'react-icons/bs'
 
 export default function FooterCom() {
   return (
-    <Footer container className="border border-t-8 border-cyan-500">
+    <Footer container className="border border-t-6 border-rose-500 dark:border-indigo-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-around sm:flex md:grid-cols-1">
           <div className="mt-5">
