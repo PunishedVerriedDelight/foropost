caee70baac044e0050c3ddabe0ce669b772a63fd
PunishedVerriedDelight <acxelfer1@gmail.com>
2024-11-06 15:29:44 -0600

    add show more functionality to the posts results of the dashboard

--- api/addCategories.js
+++ api/addCategories.js
@@ -5,10 +5,11 @@ const addCategories = async () => {
     await mongoose.connect(JWT_SECRET); 
 
     const categories = [
-        { name: "Tecnología", description: "Todo sobre las últimas innovaciones y tendencias en tecnología." },
+        /*{ name: "Tecnología", description: "Todo sobre las últimas innovaciones y tendencias en tecnología." },
         { name: "Salud", description: "Artículos relacionados con la salud y el bienestar." },
         { name: "Estilo de vida", description: "Consejos y artículos sobre estilo de vida y bienestar." },
-        { name: "Educación", description: "Recursos y artículos sobre aprendizaje y educación." }
+        { name: "Educación", description: "Recursos y artículos sobre aprendizaje y educación." }*/
+        { name: "Noticias", description: "Noticias del momento." }
     ];
 
     try {
--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,6 +15,9 @@ export default function DashPosts() {
         const data = await res.json();
         if(res.ok){
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(flase);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,22 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -64,6 +84,13 @@ export default function DashPosts() {
             </Table.Body>
           ))}
         </Table>
+        {
+          showMore && (
+            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+              Mostrar más
+            </button>
+          )
+        }
         </>
       ):(
         <p>Aun no has publicado nada</p>
