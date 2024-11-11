ab364f138e4209806660231fb348c9cce9920ee3
PunishedVerriedDelight <acxelfer1@gmail.com>
2024-11-11 02:09:19 -0600

    add pre installation

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categorie.route.js';
 import postRoutes from './routes/post.route.js'; 
 import commentRoutes from './routes/comment.route.js'; 
 import favoriteRoutes from './routes/favorite.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(
     }).catch((err) => {
         console.log(err);
     });
+
+    const __dirname = path.resolve();
 
 const app = express();
 
@@ -33,6 +36,12 @@ app.use('/api/categories', categoryRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 app.use('/api/favorite', favoriteRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
